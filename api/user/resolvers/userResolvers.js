f76e0b1467c97dbb318488bd46074f38387f4733
rodolfobandeira <contato@rodolfobandeira.com>
2021-11-11 22:14:16 -0500

    Initial commit

---
+++ api/user/resolvers/userResolvers.js
@@ -0,0 +1,9 @@
+const userResolvers = {
+  Query: {
+    users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+    user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+  }
+}
+
+module.exports = userResolvers
+
